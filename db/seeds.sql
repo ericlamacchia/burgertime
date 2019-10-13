4742297cf62a91d0c895faeabccfab5c6f7a0e29
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-10-13 10:14:52 -0400

    Created the database as well as the table and then populated the table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("hamburger",false), ("cheeseburger",false), ("impossibleburger",false), ("chiliburger",false), ("french_onion_burger",false);
